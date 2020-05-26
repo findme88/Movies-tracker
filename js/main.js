618d42551e06d067b270362c914b6d9e213b8219
zordexxx <zordexxx69@gmail.com>
2020-05-26 17:47:53 +0300

    v0.999

--- js/main.js
+++ js/main.js
@@ -1,55 +1,50 @@
-'use strict'
+"use strict";
 
-const API_KEY = '29bb47b7552ec502eb87cebfbc77f766';
-const API_URL = 'https://api.themoviedb.org/3';
-const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+const API_KEY = "29bb47b7552ec502eb87cebfbc77f766";
+const API_URL = "https://api.themoviedb.org/3";
+const IMG_URL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2";
 
-$(document).ready(function() {
-  
+$(document).ready(function () {
   // events
-  $('.search__btn').click(() => {
-    getMovie()
-  })
+  $(".search__btn").click(() => {
+    getMovie();
+  });
 
-  $('.search__field').keypress((e) => {
-    if(e.keyCode === 13)
-      getMovie()
-  })
+  $(".search__field").keypress((e) => {
+    if (e.keyCode === 13) getMovie();
+  });
 
-  
-  
   // Functions
   function getMovie() {
-    let query = $('.search__field').val()
+    let query = $(".search__field").val();
 
-    $('body').addClass('loading')
-    
-    if (query !== '') {
-      $('.movie').remove()
+    $("body").addClass("loading");
+
+    if (query !== "") {
+      $(".movie").remove();
 
       $.ajax({
         url: `${API_URL}/search/movie`,
-        type: 'GET',
-        dataType: 'json',
+        type: "GET",
+        dataType: "json",
         data: {
           api_key: API_KEY,
-          query: query
-        }
+          query: query,
+        },
       }).then((res) => {
         if (res.results.length === 0) {
-          alert('No movies found')
+          alert("No movies found");
         } else {
           res.results.forEach((movie) => {
             if (movie.poster_path !== null)
-              $('.movies').append(drawMovie(movie))
-              let $movied = $('.movies').find(`.${movie.id}`)
-              $movied.click(()=>getReviews(movie.id))
-            })
+              $(".movies").append(drawMovie(movie));
+            let $movied = $(".movies").find(`.${movie.id}`);
+            $movied.click(() => getReviews(movie.id));
+          });
         }
-        $('body').removeClass('loading')
-      })
+        $("body").removeClass("loading");
+      });
     }
-    
   }
 
   function drawMovie(movie) {
@@ -62,50 +57,48 @@ $(document).ready(function() {
                         <h3><b>Rating: </b>${movie.vote_average}</h3>
                         <p>${movie.overview}</p>
                         </div>
-                    </div>`
-                    return movieDOM
-                  }
-                  
+                    </div>`;
+    return movieDOM;
+  }
+
   function getReviews(id) {
-    console.log(id)
-    
+    console.log(id);
+
     $.ajax({
       url: `${API_URL}/movie/${id}`,
-      type: 'GET',
-      dataType: 'json',
+      type: "GET",
+      dataType: "json",
       data: {
-        api_key: API_KEY
-      }
-    }).then(res => {drawReviews(res)})
-    
+        api_key: API_KEY,
+      },
+    }).then((res) => {
+      drawReviews(res);
+    });
   }
-  
 
   function drawReviews(movie) {
-    console.log(movie.overview)
-    if (movie.overview == '') {
-    $('.window').addClass('hide-off')
-    $('.reviews__title').text(`NO REVIEW`)
-  } else {
-    $('.window').addClass('hide-off')
-    $('.reviews__title').text(movie.title)
-    $('.reviews__info').text(movie.overview)
+    if (movie.overview == "") {
+      $(".window").addClass("hide-off");
+      $(".reviews__title").text(`NO REVIEW`);
+    } else {
+      $(".window").addClass("hide-off");
+      $(".reviews__title").text(movie.title);
+      $(".reviews__info").text(movie.overview);
+    }
   }
-  }
-  
-  function removeReviews() {
-    $('.window').removeClass('hide-off')
-  }
-  
-  $('.reviews__close').click(()=>{
-    removeReviews()
-  })
 
-  $('.window').mouseup(function (e){ 
-    let div = $(".reviews"); 
-    if (!div.is(e.target) 
-        && div.has(e.target).length === 0) { 
-      removeReviews() 
+  function removeReviews() {
+    $(".window").removeClass("hide-off");
+  }
+
+  $(".reviews__close").click(() => {
+    removeReviews();
+  });
+
+  $(".window").mouseup(function (e) {
+    let div = $(".reviews");
+    if (!div.is(e.target) && div.has(e.target).length === 0) {
+      removeReviews();
     }
   });
-})
+});
